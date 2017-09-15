e0ef1723962522346e3c3ebdcff1cb8391279023
moumitawrc <moumita@wrctechnologies.com>
2017-09-15 15:46:15 +0530

    commit by moumita

--- routes/master-contact.js
+++ routes/master-contact.js
@@ -16,13 +16,8 @@ module.exports = function(app, models) {
 			},
 			include:[{model: models.firm}, {model: models.attorney}, {model: models.country},{model: models.state},{model: models.city},{model:models.designation},{model:models.industrytype}]
 		}).then(function(mastercontact){
-			var result = JSON.parse(JSON.stringify(mastercontact));
-			console.log(result[0]);
-			res.render('admin/master-contact/index',
-			{
-				layout:'dashboard',
-				master_contacts:result
-			});
+			console.log(mastercontact);
+			res.render('admin/master_contact/index',{layout:'dashboard', master_contacts:mastercontact});
 		});
 	});
 
@@ -46,7 +41,7 @@ module.exports = function(app, models) {
 		]).then(function(values){
 			var result = JSON.parse(JSON.stringify(values));
 
-			res.render('admin/master-contact/add',
+			res.render('admin/master_contact/add',
 				{
 					layout: 'dashboard',
 					countries: result[0],
@@ -68,51 +63,47 @@ module.exports = function(app, models) {
 		} else if((company_name) && first_name == 'NULL'){
 			var type = 'O';
 		}
-				models.mastercontact.create({
-					firm_id: req.body.firm_id,
-					attorney_id: req.body.attorney_id,
-					code: req.body.code,
-					designation_id: req.body.designation_id,
-					first_name: req.body.first_name,
-					last_name: req.body.last_name,
-					type: type,
-					dob: req.body.dob,
-					gender: req.body.gender,
-					industry_type: req.body.industry_type,
-					social_security_no: req.body.social_security_no,
-					company_name: req.body.company_name,
-					address_line_1: req.body.address_line_1,
-					address_line_2: req.body.address_line_2,
-					address_line_3: req.body.address_line_3,
-					country_id: req.body.country_id,
-					city_id: req.body.state_id,
-					state_id: req.body.city_id,
-					postal_code: req.body.zipcode,
-					email: req.body.email,
-					phone: req.body.phone,
-					fax: req.body.fax,
-					mobile_cell: req.body.mobile_cell,
-					website_url: req.body.website_url,
-					social_media_url: req.body.social_media_url,
-					twitter: req.body.twitter,
-					linkedin: req.body.linkedin,
-					youtube: req.body.youtube,
-					google: req.body.google,
-					im: req.body.im,
-					association_status: req.body.association_status,
-					remarks_notes: req.body.remarks_notes,
-					status:1,
-				record_type: 'M'
-			}).then(function(mastercontact){
-				res.redirect('/admin/master-contact');
-			}).catch(function(err){
-				var validation_error = err.errors;
-					res.render('admin/master-contact/add', {
-							layout: 'dashboard',
-							error_message: validation_error[0].message,
-							body: req.body
-						});
-			});
+
+	models.mastercontact.create({
+		firm_id: req.body.firm_id,
+		attorney_id: req.body.attorney_id,
+		code: req.body.code,
+		designation_id: req.body.designation_id,
+		first_name: req.body.first_name,
+		last_name: req.body.last_name,
+		type: type,
+		dob: req.body.dob,
+		gender: req.body.gender,
+		industry_type: req.body.industry_type,
+		social_security_no: req.body.social_security_no,
+		company_name: req.body.company_name,
+		address_line_1: req.body.address_line_1,
+		address_line_2: req.body.address_line_2,
+		address_line_3: req.body.address_line_3,
+		country_id: req.body.country_id,
+		city_id: req.body.state_id,
+		state_id: req.body.city_id,
+		postal_code: req.body.zipcode,
+		email: req.body.email,
+		phone: req.body.phone,
+		fax: req.body.fax,
+		mobile_cell: req.body.mobile_cell,
+		website_url: req.body.website_url,
+		social_media_url: req.body.social_media_url,
+		twitter: req.body.twitter,
+		linkedin: req.body.linkedin,
+		youtube: req.body.youtube,
+		google: req.body.google,
+		im: req.body.im,
+		association_status: req.body.association_status,
+		remarks_notes: req.body.remarks_notes,
+		status:1,
+	record_type: 'M'
+}).then(function(mastercontact){
+	res.redirect('/admin/master_contact');
+}).catch(function(err){
+	console.log(err);
+});
 
 	});
 };
--- routes/target.js
+++ routes/target.js
@@ -4,6 +4,86 @@ module.exports = function(app, models) {
 	});
 
 	app.get('/admin/target/add', function(req, res){
-			res.render('admin/target/add',{layout:'dashboard'});
+		Promise.all([
+			models.country.findAll(),
+			models.industrytype.findAll({attributes: ['id', 'industry']}),
+			models.designation.findAll({attributes: ['id', 'designation']}),
+			models.admin.findAll({
+				where: {
+					role_code: 'ATTR'
+				},
+				attributes: ['id', 'first_name','last_name']
+			}),
+			models.firm.findAll({
+				where:{
+					user_id: req.user.id
+				},
+				attributes : ['id', 'name']
+			})
+		]).then(function(values){
+			var result = JSON.parse(JSON.stringify(values));
+
+			res.render('admin/target/add',{layout:'dashboard',succ_add_msg:req.flash('succ_add_msg')[0], countries:result[0], industry_types:result[1], designations:result[2], attorney:result[3], firm:result[4]});
+			
+		});
+	});
+
+	app.post('/admin/target/add', function(req, res){
+		var first_name = req.body.first_name;
+		var last_name = req.body.last_name;
+		var company_name = req.body.company_name;
+		if((first_name) && (last_name) && (company_name)){
+			var type = 'I';
+		} else if((company_name) && first_name == 'NULL'){
+			var type = 'O';
+		}
+
+		models.mastercontact.create({
+			firm_id: req.body.firm_id,
+			attorney_id: req.body.attorney_id,
+			code: req.body.code,
+			designation_id: req.body.designation_id,
+			first_name: req.body.first_name,
+			last_name: req.body.last_name,
+			type: type,
+			dob: req.body.dob,
+			gender: req.body.gender,
+			industry_type: req.body.industry_type,
+			social_security_no: req.body.social_security_no,
+			company_name: req.body.company_name,
+			address_line_1: req.body.address_line_1,
+			address_line_2: req.body.address_line_2,
+			address_line_3: req.body.address_line_3,
+			country_id: req.body.country_id,
+			city_id: req.body.state_id,
+			state_id: req.body.city_id,
+			postal_code: req.body.zipcode,
+			email: req.body.email,
+			phone: req.body.phone,
+			fax: req.body.fax,
+			mobile_cell: req.body.mobile_cell,
+			website_url: req.body.website_url,
+			social_media_url: req.body.social_media_url,
+			twitter: req.body.twitter,
+			linkedin: req.body.linkedin,
+			youtube: req.body.youtube,
+			google: req.body.google,
+			im: req.body.im,
+			association_status: req.body.association_status,
+			remarks_notes: req.body.remarks_notes,
+			status:1,
+			record_type: 'T'
+		}).then(function(target){
+			req.flash('succ_add_msg', 'Target added successfully');
+			res.redirect('/admin/target');
+		}).catch(function(err){
+			var validation_error = err.errors;
+            res.render('admin/target/add', {
+                layout: 'dashboard',
+                error_message: validation_error[0].message,
+                body: req.body
+            });
+		});
+
 	});
 };
